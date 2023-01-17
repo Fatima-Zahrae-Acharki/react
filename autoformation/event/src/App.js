64844ac3fab0e371407a43a77f8dc284324025e7
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-01-17 12:42:30 +0100

    functional/class components event

--- autoformation/event/src/App.js
+++ autoformation/event/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import ClassClick from './components/ClassClick';
+import FunctionClick from './components/FunctionClick';
 
 function App() {
   return (
     <div className="App">
-      
+      <FunctionClick />
+      <br></br>
+      <ClassClick />
     </div>
   );
 }
---
+++ autoformation/event/src/components/FunctionClick.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+//eventHandler is a function and not a function caller (dont call it with parenthesies)
+
+function FunctionClick() {
+
+    function clickHandler(){
+        console.log('clickHandler hhhhhh');
+    }
+  return (
+    <div>
+        <button onClick={clickHandler} >Click</button>
+    </div>
+  )
+}
+
+export default FunctionClick

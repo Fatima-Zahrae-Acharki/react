956e1ca2f332b4f114ab2b0376a36a4a0ff7d22e
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-01-31 11:13:55 +0100

    parent/child components

--- autoformation/methods-as-props/src/components/ChildComponent.js
+++ autoformation/methods-as-props/src/components/ChildComponent.js
@@ -3,7 +3,11 @@ import React from 'react'
 function ChildComponent(props) {
   return (
     <div>
-        <button onClick={props.greetHandler} >Greet Parent</button>
+         {/* <button onClick={props.greetHandler} >Greet Parent</button> */}
+
+        {/* arrow function */}
+        {/* props object and pass parameter () */}
+        <button onClick={() => props.greetHandler('child')} >Greet Parent</button>
     </div>
   )
 }
--- autoformation/methods-as-props/src/components/ParentComponent.js
+++ autoformation/methods-as-props/src/components/ParentComponent.js
@@ -11,17 +11,19 @@ class ParentComponent extends Component {
       }
       this.greetParent = this.greetParent.bind(this)
     }
-
+    //defining the component
     greetParent(){
         //regular concatenation
         // alert('hello' + this.state.parentName)
         //es6
-        alert(`hello ${this.state.parentName}`)
+        // alert(`hello ${this.state.parentName}`)
+        alert(`hello ${this.state.parentName} from ${childName}`)
     }
     
   render() {
     return (
       <div>
+        {/* pass the method as prop */}
         <ChildComponent greetHandler={this.greetParent} />
       </div>
     )

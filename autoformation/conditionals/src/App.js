6e62f3d2a0a53490133981e2052f9bb1f25fb278
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-02-01 10:13:39 +0100

    conditionals

---
+++ autoformation/conditionals/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './components/UserGreeting';
+
+function App() {
+  return (
+    <div className="App">
+     <UserGreeting />
+    </div>
+  );
+}
+
+export default App;

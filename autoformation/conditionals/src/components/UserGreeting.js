450daafbe37cb6c590fe7ce753e99fc8f6c51cb8
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-02-01 13:18:32 +0100

    var inside render-ternary operator-short cercuit

--- autoformation/conditionals/src/components/UserGreeting.js
+++ autoformation/conditionals/src/components/UserGreeting.js
@@ -30,15 +30,39 @@ class UserGreeting extends Component {
         // }
 
 
-    return (
-      <div>
-        <div>welcome kirby</div>
-        <div>welcome guest</div>
-      </div>
+    // return (
+    //   <div>
+    //     <div>welcome kirby</div>
+    //     <div>welcome guest</div>
+    //   </div>
       
-    )
+    // )
 
 
+        /////////////declare variable inside render
+
+    // let message //stores the element to be rendered
+    // if(this.state.isLoggedIn) {
+    //     message = <div>welcome kirby</div>
+    // }else{
+    //     message = <div>welcome guest</div>
+    // }
+    // return <div>{message}</div>
+
+
+
+    /////////ternary conditional operator (can be used inside jsx (simple))
+
+    // return(
+    //     this.state.isLoggedIn ? //if true, return:
+    //     <div>welcome kirby</div> : //else 
+    //     <div>welcome guest</div>
+    // )
+
+
+    //short circuit operator (specific case of the ternary conditional operator above)
+        return this.state.isLoggedIn && <div>welcome kirby</div>
+  }
 }
 
 export default UserGreeting

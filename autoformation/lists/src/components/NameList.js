cba5623f96fd9a185209e3f30fb0ee35a47cc4d1
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-02-02 11:37:56 +0100

    map method

--- autoformation/lists/src/components/NameList.js
+++ autoformation/lists/src/components/NameList.js
@@ -3,12 +3,21 @@ import React from 'react'
 
 //repeat some html for each item in the list
 function NameList() {
-    const names = ['midoriya', 'todoroki', 'bakugo',]
+    const names = ['midoriya', ' todoroki', ' bakugo',]
+    const nameList = names.map(name => <h2>{name}</h2> )//map() takes in a function as an argument that we will be using an arrow function
   return (
     <div>
-        <h2>{names[0]}</h2>
+        {/* 1 */}
+
+        {/* <h2>{names[0]}</h2>
         <h2>{names[1]}</h2>
-        <h2>{names[2 ]}</h2>
+        <h2>{names[2 ]}</h2> */}
+
+        {/* 2 */}
+
+        {
+        <div>{nameList}</div>
+        }
     </div>
   )
 }
